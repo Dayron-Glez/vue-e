00a768af1cd9a267a051a54d5fd0eb07c947fc0f
marcosdgi <marcosdgi@softel.cu>
2024-06-02 18:50:59 -0400

    Creating Page Products

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { createMemoryHistory, createRouter } from 'vue-router'
-
 import HomePage from './pages/HomePage.vue'
 import AnotherPage from './pages/AnotherPage.vue'
+import Products from './pages/Products.vue'
 
 const routes = [
   { path: '/', component: HomePage },
   { path: '/another', component: AnotherPage },
+  { path: '/products', component: Products },
 ]
 
 const router = createRouter({
---
+++ src/pages/Products.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template lang="pug">
+.flex.justify-center.items-center
+    input( type="text" placeholder="Buscar Aquí")
+    button(class="buttonSearch") Buscar
+
+    div( class="container-products") No hay Productos actualmente...
+</template>
+
+<style scoped>
+.buttonSearch{
+    @apply border rounded-[8px] w-10 fixed bg-red-400 ;
+}
+.container-products{
+    @apply flex-col justify-around ;
+}
+
+</style>
