7c3a290eb6f9c67cdedd9e72dc73eff60fa5908b
Dayron-Glez <dayronglez01@gmail.com>
2024-06-02 18:07:02 -0400

    ADDED: vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <script setup>
 
+
 </script>
 
 <template>
-  
+
+<nav>
+    <RouterLink to="/">Go to Home</RouterLink>
+    <RouterLink to="/another">Go to another</RouterLink>
+  </nav>
+  <RouterView />
+
 </template>
